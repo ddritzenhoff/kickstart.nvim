a5c872ef491a3844ea98507c4403460cd3f81426
Dominik Ritzenhoff <dominik@ritzenhoff.dev>
2026-01-11 13:29:53 +0100

    just copy primeagen's keybinds and vim.opts

--- init.lua
+++ init.lua
@@ -1,71 +1,91 @@
+-- Leaders
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = true
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-vim.opt.autoindent = true
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.confirm = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.encoding = 'utf8'
-vim.opt.ruler = true
-vim.opt.expandtab = true
-vim.opt.history = 10000
-vim.opt.smarttab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.showcmd = true
-vim.opt.wildmenu = true
-vim.opt.virtualedit = 'block'
-vim.opt.termguicolors = true
+-- Cursor
+vim.opt.guicursor = '' -- Block cursor in all modes
+
+-- Line numbers
+vim.opt.nu = true -- Show absolute line numbers
+vim.opt.relativenumber = true -- Show relative line numbers for easy motions
+
+-- Tabs & indentation
+vim.opt.tabstop = 4 -- A tab character = 4 spaces
+vim.opt.softtabstop = 4 -- Tab key inserts 4 spaces
+vim.opt.shiftwidth = 4 -- Indent commands use 4 spaces
+vim.opt.expandtab = true -- Convert tabs to spaces
+
+-- Smart indentation
+vim.opt.smartindent = true -- Auto-indent new lines based on syntax
+
+-- Line wrapping
+vim.opt.wrap = false -- Do not wrap long lines; scroll horizontally
+
+-- Swap, backup, and undo
+vim.opt.swapfile = false -- Disable swap files
+vim.opt.backup = false -- Disable backup files
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir' -- Directory for persistent undo
+vim.opt.undofile = true -- Enable persistent undo
+
+-- Search behavior
+vim.opt.hlsearch = false -- Do not highlight search results permanently
+vim.opt.incsearch = true -- Highlight matches while typing search
+
+-- Colors & display
+vim.opt.termguicolors = true -- Enable 24-bit true color support
+vim.opt.scrolloff = 8 -- Keep 8 lines visible above/below cursor
+vim.opt.signcolumn = 'yes' -- Always show sign column (for LSP, Git, etc.)
+vim.opt.isfname:append '@-@' -- Allow `@-@` in file paths for commands like gf
+
+-- Performance
+vim.opt.updatetime = 50 -- Faster update events for CursorHold, LSP, etc.
+
+-- Coding style / visual guides
+vim.opt.colorcolumn = '80' -- Highlight column 80 for line length
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- File explorer (simple + universal)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move selected lines up/down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Join lines without cursor jump
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Keep cursor centered when moving/searching
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Paste without overwriting yank
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- System clipboard yanks
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+
+-- Delete without losing yank
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
+-- Format via LSP
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+-- Quick search & replace word under cursor
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
