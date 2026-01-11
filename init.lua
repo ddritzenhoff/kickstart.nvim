f0ddbd589a82f55f3372f95216beec88f821dd63
Dominik Ritzenhoff <dominik@ritzenhoff.dev>
2026-01-11 16:20:07 +0100

    Add keybindings for lsp

--- init.lua
+++ init.lua
@@ -85,12 +85,57 @@ vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- [[ Basic Autocommands ]]
+local ddritzenhoff = vim.api.nvim_create_augroup('ddritzenhoff', {})
+
 --  See `:help lua-guide-autocommands`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank { timeout = 100 }
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = ddritzenhoff,
+  pattern = '*',
+  command = [[%s/\s\+$//e]],
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = ddritzenhoff,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set('n', 'gd', function()
+      vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set('n', 'K', function()
+      vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set('n', '<leader>vws', function()
+      vim.lsp.buf.workspace_symbol()
+    end, opts)
+    vim.keymap.set('n', '<leader>vd', function()
+      vim.diagnostic.open_float()
+    end, opts)
+    vim.keymap.set('n', '<leader>vca', function()
+      vim.lsp.buf.code_action()
+    end, opts)
+    vim.keymap.set('n', '<leader>vrr', function()
+      vim.lsp.buf.references()
+    end, opts)
+    vim.keymap.set('n', '<leader>vrn', function()
+      vim.lsp.buf.rename()
+    end, opts)
+    vim.keymap.set('i', '<C-h>', function()
+      vim.lsp.buf.signature_help()
+    end, opts)
+    vim.keymap.set('n', '[d', function()
+      vim.diagnostic.goto_prev {}
+    end, opts)
+    vim.keymap.set('n', ']d', function()
+      vim.diagnostic.goto_next {}
+    end, opts)
   end,
 })
 
